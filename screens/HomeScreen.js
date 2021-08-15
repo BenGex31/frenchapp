5a2a16a2eebb091b6657ea6dadadba1a1fcbb8ec
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-15 15:22:24 +0200

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useContext } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
 
-import { IconButton } from '../components';
-import Firebase from '../config/firebase';
-import { AuthenticatedUserContext } from '../navigation/AuthenticatedUserProvider';
+import { StatusBar } from "expo-status-bar";
+import React, { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { IconButton } from "../components";
+import Firebase from "../config/firebase";
+import { AuthenticatedUserContext } from "../navigation/AuthenticatedUserProvider";
 
 const auth = Firebase.auth();
 
 export default function HomeScreen() {
-    const { user } = useContext(AuthenticatedUserContext);
+  const { user } = useContext(AuthenticatedUserContext);
   const handleSignOut = async () => {
     try {
       await auth.signOut();
@@ -17,44 +19,44 @@ export default function HomeScreen() {
       console.log(error);
     }
   };
-  return(
+  return (
     <View style={styles.container}>
-    <StatusBar style='dark-content' />
-    <View style={styles.row}>
-      <Text style={styles.title}>Welcome {user.email}!</Text>
-      <IconButton
-        name='logout'
-        size={24}
-        color='#fff'
-        onPress={handleSignOut}
-      />
+      <StatusBar style='dark-content' />
+      <View style={styles.row}>
+        <Text style={styles.title}>Welcome {user.email}!</Text>
+        <IconButton
+          name='logout'
+          size={24}
+          color='#fff'
+          onPress={handleSignOut}
+        />
+      </View>
+      <Text style={styles.text}>Your UID is: {user.uid} </Text>
     </View>
-    <Text style={styles.text}>Your UID is: {user.uid} </Text>
-  </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#e93b81',
-        paddingTop: 50,
-        paddingHorizontal: 12
-      },
-      row: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 24
-      },
-      title: {
-        fontSize: 24,
-        fontWeight: '600',
-        color: '#fff'
-      },
-      text: {
-        fontSize: 16,
-        fontWeight: 'normal',
-        color: '#fff'
-      }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#e93b81",
+    paddingTop: 50,
+    paddingHorizontal: 12,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "600",
+    color: "#fff",
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "normal",
+    color: "#fff",
+  },
+});
