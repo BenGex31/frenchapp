b0c0b068746455e800e90f02dfc8e4b8f89497c1
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-15 13:25:17 +0200

    Import and use created custom components

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button';
+import IconButton from './components/IconButton';
+import InputField from './components/InputField';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text >Open up App.js to start working on your app!</Text>
+
+        <Button title='Button test' containerStyle={{justifyContent: 'center', alignContent: 'center', width: 200 }}/>
+
+      <IconButton name={'Solid Button'}  type="white"/>
+      <InputField 
+        placeholder='Search' 
+        containerStyle={{justifyContent: 'center', alignContent: 'center', width: 200 }} 
+        leftIcon='briefcase-search' placeholderTextColor='#000'
+      />
+      
       <StatusBar style="auto" />
     </View>
   );
