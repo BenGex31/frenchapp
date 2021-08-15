a91c51f3e21b61b1848be347e50c420388ff47c4
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-15 15:22:08 +0200

    Create index.js

---
+++ navigation/index.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import React from "react";
+
+import { AuthenticatedUserProvider } from "./AuthenticatedUserProvider";
+import RootNavigator from "./RootNavigator";
+
+/**
+ * Wrap all providers here
+ */
+
+export default function Routes() {
+  return (
+    <AuthenticatedUserProvider>
+      <RootNavigator />
+    </AuthenticatedUserProvider>
+  );
+}
