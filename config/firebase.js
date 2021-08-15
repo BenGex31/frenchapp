73e9ebeb883da935c510a5fc3d19e124885abd56
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-15 15:21:56 +0200

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,17 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import Constants from 'expo-constants';
+/** @format */
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import Constants from "expo-constants";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: Constants.manifest.extra.apiKey,
-  authDomain: Constants.manifest.extra.authDomain,
+  apiKey: "AIzaSyCew6cq38oe5-grpoBO2SnbkMuti7EO8IM",
+  authDomain: "frenchappweb-774bd.firebaseapp.com",
   projectId: Constants.manifest.extra.projectId,
   storageBucket: Constants.manifest.extra.storageBucket,
   messagingSenderId: Constants.manifest.extra.messagingSenderId,
-  appId: Constants.manifest.extra.appId
+  appId: Constants.manifest.extra.appId,
 };
 
 let Firebase;
