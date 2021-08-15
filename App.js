a75097636aa76412c9053d3f3f5e503d6e4192fe
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-15 15:22:28 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from './components/Button';
-import IconButton from './components/IconButton';
-import InputField from './components/InputField';
+/** @format */
+
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Button from "./components/Button";
+import IconButton from "./components/IconButton";
+import InputField from "./components/InputField";
+import Routes from "./navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text >Open up App.js to start working on your app!</Text>
-
-        <Button title='Button test' containerStyle={{justifyContent: 'center', alignContent: 'center', width: 200 }}/>
-
-      <IconButton name={'Solid Button'}  type="white"/>
-      <InputField 
-        placeholder='Search' 
-        containerStyle={{justifyContent: 'center', alignContent: 'center', width: 200 }} 
-        leftIcon='briefcase-search' placeholderTextColor='#000'
-      />
-      
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Routes />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
