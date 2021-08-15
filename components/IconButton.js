55b3fb2197a0be1a7b4b99ee8f327a30351cfba2
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-15 13:24:47 +0200

    Create IconButton.js

---
+++ components/IconButton.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Pressable, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+const IconButton = ({ color, size, onPress, name, type }) => {
+  return (
+    <Pressable
+      style={args => {
+        if (args.pressed) {
+          return [
+            styles.base,
+            {
+              opacity: 0.5,
+              backgroundColor: 'transparent'
+            }
+          ];
+        }
+
+        return [styles.base, { opacity: 1, backgroundColor: 'transparent' }];
+      }}
+      onPress={onPress}
+    >
+      <Button title={name} size={size} color={color} type={type} />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  base: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+export default IconButton;
