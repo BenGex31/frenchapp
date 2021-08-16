c18e8d47f163c4e72e992db30f8c3e61f435bf59
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-16 09:41:00 +0200

    Update InputField.js

--- components/InputField.js
+++ components/InputField.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+/** @format */
+
+import React from "react";
+import { View, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const InputField = ({
   leftIcon,
-  iconColor = '#000',
+  iconColor = "#000",
   rightIcon,
   inputStyle,
   containerStyle,
   placeholder,
-  placeholderTextColor = '#444',
+  placeholderTextColor = "#444",
   handlePasswordVisibility,
   ...rest
 }) => {
@@ -24,7 +26,7 @@ const InputField = ({
         />
       ) : null}
       <TextInput
-      placeholder={placeholder}
+        placeholder={placeholder}
         {...rest}
         placeholderTextColor={placeholderTextColor}
         style={[styles.input, inputStyle]}
@@ -46,21 +48,21 @@ const InputField = ({
 const styles = StyleSheet.create({
   container: {
     borderRadius: 4,
-    flexDirection: 'row',
-    padding: 12
+    flexDirection: "row",
+    padding: 12,
   },
   leftIcon: {
-    marginRight: 10
+    marginRight: 10,
   },
   input: {
     flex: 1,
-    width: '100%',
-    fontSize: 18
+    width: "100%",
+    fontSize: 18,
   },
   rightIcon: {
-    alignSelf: 'center',
-    marginLeft: 10
-  }
+    alignSelf: "center",
+    marginLeft: 10,
+  },
 });
 
 export default InputField;
