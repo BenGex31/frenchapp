5db65c85fa46c9f884c5871797aefcec443a8d0f
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-16 11:42:42 +0200

    Update IconButton.js

--- components/IconButton.js
+++ components/IconButton.js
@@ -1,39 +1,42 @@
-import React from 'react';
-import { Pressable, StyleSheet } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+/** @format */
 
+import React from "react";
+import { Pressable, StyleSheet } from "react-native";
+import { Button } from "react-native-elements";
 
 const IconButton = ({ color, size, onPress, name, type, containerStyle }) => {
   return (
     <Pressable
-      style={args => {
+      style={(args) => {
         if (args.pressed) {
           return [
             styles.base,
             {
               opacity: 0.5,
-              backgroundColor: 'transparent'
+              backgroundColor: "transparent",
             },
-            containerStyle
+            containerStyle,
           ];
         }
 
-        return [styles.base, { opacity: 1, backgroundColor: 'transparent' }];
-      }}
-      
-    >
-      <Button onPress={onPress} title={name} size={size} color={color} type={type} />
+        return [styles.base, { opacity: 1, backgroundColor: "transparent" }];
+      }}>
+      <Button
+        onPress={onPress}
+        title={name}
+        size={size}
+        color={color}
+        type={type}
+      />
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
   base: {
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default IconButton;
