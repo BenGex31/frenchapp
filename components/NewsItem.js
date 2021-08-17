0c58f643b5a40ce49506aabb2cc8e4d74fefa73b
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-17 17:51:46 +0200

    Update NewsItem.js
    Insert Modal React Native

--- components/NewsItem.js
+++ components/NewsItem.js
@@ -1,7 +1,16 @@
 /** @format */
 
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Alert,
+  Modal,
+  Pressable,
+} from "react-native";
 import moment from "moment";
 import newsImage from "../assets/news.jpeg";
 
@@ -15,28 +24,53 @@ const NewsItem = ({
   urlToImage,
   publishedAt,
 }) => {
+  const [modalVisible, setModalVisible] = useState(false);
   const correctFormatDate = moment(publishedAt).format("DD/MM/YYYY");
   return (
-    <TouchableOpacity>
-      <View style={styles.container}>
-        <View style={styles.containerTitle}>
-          {urlToImage === null ? (
-            <Image source={newsImage} style={styles.image} />
-          ) : (
-            <Image source={{ uri: urlToImage }} style={styles.image} />
-          )}
-          <View>
-            <Text style={styles.title}>{title}</Text>
-            <Text style={styles.description}>{description}</Text>
+    <View style={styles.container}>
+      <View style={styles.containerTitle}>
+        {urlToImage === null ? (
+          <Image source={newsImage} style={styles.image} />
+        ) : (
+          <Image source={{ uri: urlToImage }} style={styles.image} />
+        )}
+        <View>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.description}>{description}</Text>
+          <View style={styles.centeredView}>
+            <Modal
+              animationType='slide'
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setModalVisible(!modalVisible);
+              }}>
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <Text style={styles.modalText}>{content}</Text>
+                  <Pressable
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => setModalVisible(!modalVisible)}>
+                    <Text style={styles.textStyle}>Hide Modal</Text>
+                  </Pressable>
+                </View>
+              </View>
+            </Modal>
+            <Pressable
+              style={[styles.button, styles.buttonOpen]}
+              onPress={() => setModalVisible(true)}>
+              <Text style={styles.textStyle}>Read more...</Text>
+            </Pressable>
           </View>
         </View>
-        <View>
-          <Text style={styles.author}>by {author}</Text>
-          <Text style={styles.author}>Source: {source}</Text>
-          <Text style={styles.author}>Published at : {correctFormatDate}</Text>
-        </View>
       </View>
-    </TouchableOpacity>
+      <View>
+        <Text style={styles.author}>by {author}</Text>
+        <Text style={styles.author}>Source: {source}</Text>
+        <Text style={styles.author}>Published at : {correctFormatDate}</Text>
+      </View>
+    </View>
   );
 };
 
@@ -44,7 +78,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderWidth: 0.5,
-    borderColor: "black",
     borderRadius: 10,
     marginHorizontal: 10,
     marginVertical: 5,
@@ -83,6 +116,46 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     color: "#eaf1fb",
     fontSize: 6,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#5a3eff",
+  },
+  buttonClose: {
+    backgroundColor: "#5a3eff",
+  },
+  textStyle: {
+    color: "#38d8ff",
+    fontWeight: "bold",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
 
