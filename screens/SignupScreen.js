29174fcb374d81a67dc55aa43485c295ac3c2b3e
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-18 16:23:41 +0200

    Update SignupScreen.js
    Insert input Firstname and Lastname to signup

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -7,12 +7,14 @@ import { StyleSheet, Text, View, Button as RNButton } from "react-native";
 
 import { Button, InputField, ErrorMessage } from "../components";
 import Firebase from "../config/firebase";
+import "firebase/firestore";
 
 const auth = Firebase.auth();
 
 export default function SignupScreen({ navigation }) {
   const [email, setEmail] = useState("");
-  const [lastname, setLastname] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVisibility, setPasswordVisibility] = useState(true);
   const [rightIcon, setRightIcon] = useState("eye");
@@ -32,6 +34,17 @@ export default function SignupScreen({ navigation }) {
     try {
       if (email !== "" && password !== "") {
         await auth.createUserWithEmailAndPassword(email, password);
+        const currentUser = Firebase.auth().currentUser;
+        currentUser.updateProfile({
+          displayName: firstname + " " + lastname
+        })
+
+        /*const db = Firebase.firestore();
+        db.collection("users").doc(currentUser.uid).set({
+          email: currentUser.email,
+          lastname: lastname,
+          firstname: firstname,
+        });*/
       }
     } catch (error) {
       setSignupError(error.message);
@@ -51,7 +64,7 @@ export default function SignupScreen({ navigation }) {
           marginBottom: 20,
         }}
         leftIcon='email'
-        placeholder='Enter email'
+        placeholder='Email'
         autoCapitalize='none'
         keyboardType='email-address'
         textContentType='emailAddress'
@@ -68,7 +81,7 @@ export default function SignupScreen({ navigation }) {
           marginBottom: 20,
         }}
         leftIcon='lock'
-        placeholder='Enter password'
+        placeholder='Password'
         autoCapitalize='none'
         autoCorrect={false}
         secureTextEntry={passwordVisibility}
@@ -86,32 +99,44 @@ export default function SignupScreen({ navigation }) {
           backgroundColor: "#fff",
           marginBottom: 20,
         }}
-        // leftIcon='lock'
-        placeholder='Enter lastname'
+        leftIcon='account'
+        placeholder='Firstname'
         autoCapitalize='none'
         autoCorrect={false}
-        // secureTextEntry={passwordVisibility}
-        // textContentType='password'
-        // rightIcon={rightIcon}
+        value={firstname}
+        onChangeText={(text) => setFirstName(text)}
+      />
+      <InputField
+        inputStyle={{
+          fontSize: 14,
+        }}
+        containerStyle={{
+          backgroundColor: "#fff",
+          marginBottom: 20,
+        }}
+        leftIcon='account'
+        placeholder='Lastname'
+        autoCapitalize='none'
+        autoCorrect={false}
         value={lastname}
-        onChangeText={(text) => setLastname(text)}
-        // handlePasswordVisibility={handlePasswordVisibility}
+        onChangeText={(text) => setLastName(text)}
       />
       {signupError ? <ErrorMessage error={signupError} visible={true} /> : null}
       <Button
         onPress={onHandleSignup}
         backgroundColor='#f57c00'
         title='Signup'
-        tileColor='#fff'
+        tileColor='white'
         titleSize={20}
         containerStyle={{
           marginBottom: 24,
+          backgroundColor: "#0b85ff",
         }}
       />
       <RNButton
         onPress={() => navigation.navigate("Login")}
         title='Go to Login'
-        color='#fff'
+        color='#0d204b'
       />
     </View>
   );
@@ -120,15 +145,18 @@ export default function SignupScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#e93b81",
+    backgroundColor: "#eaf1fb",
     paddingTop: 50,
     paddingHorizontal: 12,
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 24,
-    fontWeight: "600",
-    color: "#fff",
+    fontWeight: "700",
+    color: "#0d204b",
     alignSelf: "center",
     paddingBottom: 24,
+    fontStyle: "italic",
   },
 });
