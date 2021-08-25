a6ff44fce54438377fd24f29d7e5f656459e39ac
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-25 12:27:35 +0200

    Update App.js
    remove unused components and styles

--- App.js
+++ App.js
@@ -1,22 +1,8 @@
 /** @format */
 
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Button from "./components/Button";
-import IconButton from "./components/IconButton";
-import InputField from "./components/InputField";
 import Routes from "./navigation";
 
 export default function App() {
   return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
